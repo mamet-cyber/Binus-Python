f89c9aef6bf85cf5f6ee59f2ce21c0730ade6308
mamet <mametiopc@gmail.com>
2025-12-21 07:07:48 +0700

    Update tugas3.py

--- Pertemuan 3/tugas3.py
+++ Pertemuan 3/tugas3.py
@@ -6,25 +6,26 @@ if (x>0):
 else:
     print("bilangan genap")
 
+
 #Exercise2 age
 age =int(input("age="))
-if((age<=1)):
+if(age<=1):
     print ("Baby")
-elif((age>=1)) and (age<=3):
+elif((age>=1) and (age<=3)):
     print("Toodler")
-elif((age>=3)) and (age<=5):
+elif((age>=3) and (age<=5)):
     print("Prescholer")
-elif((age>=5)) and (age<=12):
+elif((age>=5) and (age<=12)):
     print("Child")
-elif((age>=12)) and (age<=17):
+elif((age>=12) and (age<=17)):
     print("Teenager")
-elif((age>=17)) and (age<=21):
+elif((age>=17) and (age<=21)):
     print("Young Adult")
-elif((age>=21)) and (age<=30):
+elif((age>=21) and (age<=30)):
     print("Pre-adult")
-elif((age>=30)) and (age<=50):
+elif((age>=30) and (age<=50)):
     print("Adult")
-elif((age>=50)) and (age<=70):
+elif((age>=50) and (age<=70)):
     print("Pre-elderly")
 else:
     print("Ederly")
@@ -54,12 +55,15 @@ elif(deg=="kelvin"):
 a=float(input("panjang sisi a= "))
 b=float(input("panjang sisi b= "))
 c=float(input("panjang sisi c= "))
+import math
 if(a==b==c):
     print("maka segitiga sama sisi")
-elif((a==b) or (a==c)):
+elif((a==b) or (a==c) or (b==c)):
     print("maka segitiga sama kaki ")
 elif((a+b<c) or (a+c<b) or (b+c<a)):
     print("maka bukan segitiga")
+elif(a==math.sqrt(b**2+c**2) or (b==math.sqrt(c**2+a**2)) or (c==math.sqrt(a**2+b**2))):
+    print("maka segitiga siku-siku")
 else:
     print("maka segitiga sembarang")
 
@@ -77,5 +81,5 @@ else:
         print("maka dua akar yang berbeda")
     elif(D<0):
         print("maka akar imajiner")
-    elif(D==0):
+    else:
         print("maka akar ganda")
